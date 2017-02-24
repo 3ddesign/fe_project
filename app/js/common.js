bcd6b956724a16a68cd0985254d59fe018b6ce24
Dmitry Scherbakov <3ddesign@bk.ru>
2017-02-24 16:53:19 +0200

    fe 0.1

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,7 @@ for (var i = 0; i < lis.length; i++) {
     lis[i].style.position = 'relative';
     var span = document.createElement('span');
     span.style.cssText = 'position:absolute;left:0;top:0';
-  
+
     lis[i].appendChild(span);
 }
 var width = 301;
