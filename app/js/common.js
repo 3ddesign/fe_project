e9e8fbbb1ccff6124bdac00f71e492f83a619c2c
Dmitry Scherbakov <3ddesign@bk.ru>
2017-03-01 11:00:04 +0200

    fe 0.2

--- app/js/common.js
+++ app/js/common.js
@@ -1,39 +1,37 @@
 var button = document.getElementById("search");
 var container = document.getElementById("photogrid");
 
-var value = '';
+//random value
+var arr = ["travel", "wine", "blue", "cars"];
+var value = (arr[Math.round(Math.random() * (arr.length - 1))]);
 
-button.onclick = function(e) {
-    e.preventDefault();
-    value = document.getElementById("searchfield").value;
-    getUrls(value);
-};
-
-
+//request to pixabay
 let getUrls = function(val) {
     let URL = "https://pixabay.com/api/?key=" + '4341489-c7135f07e924eb271481ce96f' + "&q=" + encodeURIComponent(val);
     let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
     let xhr = new XHR();
-
     xhr.open('GET', URL, true);
-
     xhr.onload = function() {
         let pics = JSON.parse(this.responseText);
-        let x = pics.hits[1].webformatURL;
-        let n = pics.hits[1].tags.split(',')[0];
-        console.log(x);
-        console.log(n);
-
         container.innerHTML = '';
-        container.innerHTML = '<figure class="tile"><img class="grid_img" src="' + x + '" alt="image"><figcaption class="tile__tag">' + n + '</figcaption></figure>';
-
-        return x;
-
+        for (var i = 0; i < 7; i++) {
+            var figure = document.createElement('figure');
+            figure.className = "cell";
+            figure.innerHTML = '<img class="cell__img" src="' + pics.hits[i].webformatURL + '" alt="image"><figcaption class="cell__name">' + pics.hits[i].tags.split(',')[0] + '</figcaption>';
+            container.appendChild(figure);
+        }
     };
-
     xhr.onerror = function() {
         console.log('Ошибка ' + this.status);
     };
 
     xhr.send();
+};
+
+//handler for search button
+getUrls(value);
+button.onclick = function(e) {
+    e.preventDefault();
+    value = document.getElementById("searchfield").value;
+    getUrls(value);
 };
